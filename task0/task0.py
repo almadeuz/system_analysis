53da66c608462ae5d36d4273ce15d25085edeae6
almadeuz <146022417+almadeuz@users.noreply.github.com>
2025-09-17 21:56:58 +0300

    Update task0.py

--- task0/task0.py
+++ task0/task0.py
@@ -1,5 +1,10 @@
 import csv
+import numpy as np
 
 path = "/task2.csv"
+node = []
+with open(path, 'r') as taskfile:
+    iter = csv.reader(taskfile)
+    for i in iter:
+        node.append((int(i[0]),int(i[1])))
 #def main(path):
-
