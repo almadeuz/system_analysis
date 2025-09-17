87e56c27abe447b862636f39014f70fcb3faac2d
almadeuz <146022417+almadeuz@users.noreply.github.com>
2025-09-17 21:59:00 +0300

    Update task0.py

--- task0/task0.py
+++ task0/task0.py
@@ -7,4 +7,7 @@ with open(path, 'r') as taskfile:
     iter = csv.reader(taskfile)
     for i in iter:
         node.append((int(i[0]),int(i[1])))
+    size = max(max(node))
+    matrix = np.zeros((size,size), dtype=np.int16)
+    print(matrix)
 #def main(path):
