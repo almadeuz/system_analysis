862221ddc20ebf5d7673493077324adaea1179fb
almadeuz <146022417+almadeuz@users.noreply.github.com>
2025-09-17 22:15:33 +0300

    Update task0.py

--- task0/task0.py
+++ task0/task0.py
@@ -1,15 +1,15 @@
 import csv
 import numpy as np
 
-def main(path = "/task2.csv"):
+def main(csvfile):
     node = []
-    with open(path, 'r') as taskfile:
-        iter = csv.reader(taskfile)
-        for i in iter:
-            node.append((int(i[0]),int(i[1])))
-        size = max(max(node))
-        matrix = np.zeros((size,size), dtype=np.int16)
-        for a, b in node:
-            matrix[a-1][b-1] = 1
-            matrix[b-1][a-1] = 1
-        return matrix
+    iter = csv.reader(csvfile.splitlines())
+    for i in iter:
+        print(i)
+        node.append((int(i[0]),int(i[1])))
+    size = max(max(node))
+    matrix = np.zeros((size,size), dtype=np.int16)
+    for a, b in node:
+        matrix[a-1][b-1] = 1
+        matrix[b-1][a-1] = 1
+    return matrix
